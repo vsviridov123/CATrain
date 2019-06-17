4ff175f770fd5d1909f55e17ac27d8929b1de97e
Vladislav <vlad_cs_real98@mail.ru>
2019-06-17 13:00:58 +0400

    Initial Commit

---
+++ CoreAnimationTrain/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreAnimationTrain
+//
+//  Created by Владислав Свиридов on 17/06/2019.
+//  Copyright © 2019 Vladislav Sviridov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
